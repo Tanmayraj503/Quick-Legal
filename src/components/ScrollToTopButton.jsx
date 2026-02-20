abd1b75a55b320878a9750c2c384e446e1ecd0a4
unknown <tanmayraj707@gmail.com>
2026-02-20 11:53:34 +0530

    changes

--- src/components/ScrollToTopButton.jsx
+++ src/components/ScrollToTopButton.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-export default function ScrollToTopButton(){
-     const [isVisible, setIsVisible] = useState(false);
+export default function ScrollToTopButton() {
+    const [isVisible, setIsVisible] = useState(false);
 
-     
-      const toggleVisibility = () => {
+
+    const toggleVisibility = () => {
         if (window.pageYOffset > 200) {
             setIsVisible(true);
         } else {
@@ -13,7 +13,25 @@ export default function ScrollToTopButton(){
         }
     };
 
-    return(
-        <h1>teri maa ki</h1>
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', toggleVisibility);
+        return () => window.removeEventListener('scroll', toggleVisibility);
+    })
+
+    return (
+        isVisible && (
+            <button
+                onClick={scrollToTop}
+                className="scroll-to-top h-12 w-12 fixed bottom-5 right-5 bg-[#0f2027] text-white rounded-full shadow-lg hover:[#203a43] transition-opacity duration-300 ease-in-out flex items-center justify-center z-5">
+                ↑
+            </button>
+        )
     )
 }
